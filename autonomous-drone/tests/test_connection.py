69fb07e3be7a1107a011aa000d1d0d4e65ea54a1
LeaSponem <leasponem1@gmail.com>
2022-03-06 22:19:45 +0100

    Modified connection test using class InspectionDrone

--- autonomous-drone/tests/test_connection.py
+++ autonomous-drone/tests/test_connection.py
@@ -1,23 +1,17 @@
-from dronekit import VehicleMode, connect
 import time
+from drone.inspection_drone import InspectionDrone
 
 
-def connection_drone(connection_string, baudrate):
-    print("Trying to connect to the drone")
-    vehicle = connect(connection_string, baudrate, wait_ready=True)
-    print("Success")
-    return vehicle
-
-
-# Connection between the Raspberry Pi and the Pixhawk
-drone = connection_drone('/dev/serial0', baudrate=115200)
+# Connection between the Raspberry and the Pixhawk
+drone = InspectionDrone('/dev/serial0', baudrate=115200,
+                        two_way_switches=[7, 8], three_way_switches=[5, 6, 9, 10, 11, 12])
 
 # Access to parameters
 for _ in range(10):
-    print("Drone velocity :", drone.velocity)
+    print("Drone velocity :", drone.vehicle.velocity)
     time.sleep(1)
 
 # Change mode
-print("Actual mode:", drone.mode)
-drone.mode = VehicleMode("GUIDED")
-print("New mode:", drone.mode)
+print("Actual mode:", drone.vehicle.mode)
+drone.set_guided_mode()
+print("New mode:", drone.vehicle.mode)
