91ce665a5594555f4e6a7a2f82a0c353f9db0a2c
LeaSponem <leasponem1@gmail.com>
2022-03-07 16:05:44 +0100

    Modified going forward test

--- autonomous-drone/tests/test_go_forward.py
+++ autonomous-drone/tests/test_go_forward.py
@@ -1,31 +1,25 @@
-from drone.inspection_drone import InspectionDrone
+from inspection_drone import InspectionDrone
 import time
 
-guided_mode_switch = 6
 start_mission_switch = 8
 
 drone = InspectionDrone('/dev/serial0', baudrate=115200,
                         two_way_switches=[7, 8], three_way_switches=[5, 6, 9, 10, 11, 12])
 
 last_flight_mode = drone.vehicle.mode
+test_time=4
+total_test_time = 4 * 2 + test_time
 
-while True:
+while not drone.mission_running():
     drone.update_switch_states()
-    if drone.switches[start_mission_switch].is_up() and not drone.mission_running():
+    if drone.switches[start_mission_switch].is_up():
+        start_time = time.time()
+        time_elapsed = time.time() - start_time
         drone.launch_mission()
         print("Starting mission")
-    if drone.mission_running():
-        if drone.switches[guided_mode_switch].is_up() and not drone.is_in_guided_mode():
-            last_flight_mode = drone.vehicle.mode
-            drone.set_guided_mode()
-            print("Guided mode")
-        if drone.switches[guided_mode_switch].is_down() and drone.is_in_guided_mode():
-            drone.set_flight_mode(last_flight_mode)
-            print("Taking back control")
-        if drone.is_in_guided_mode():
-            drone.send_mavlink_go_forward(0.25)
-            print("Going forward")
-            time.sleep(3)
-            drone.send_mavlink_stay_stationary()
-            print("Stop")
-            time.sleep(5)
+while time_elapsed < total_test_time:
+    time_elapsed = time.time() - start_time
+    if time_elapsed < 4+test_time:
+        drone.send_mavlink_go_forward(0.5)
+    if 4+test_time < time_elapsed < total_test_time:
+        drone.send_mavlink_go_backward(0.5)
