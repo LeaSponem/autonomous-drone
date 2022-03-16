6932191e58b56f42aa8dd28dfb8b56cc3f37688c
LeaSponem <leasponem1@gmail.com>
2022-03-16 10:36:55 +0100

    Modified test go forward

--- autonomous-drone/tests/test_go_forward.py
+++ autonomous-drone/tests/test_go_forward.py
@@ -1,25 +1,41 @@
-from inspection_drone import InspectionDrone
+# cd C:\Users\leasp\Documents\ENPC\drone_autonome\autonomous-drone\autonomous-drone\tests
+# python test_go_forward.py --connect udp:127.0.0.1:14551
+import sys
 import time
+sys.path.insert(0, '../drone')
+from inspection_drone import InspectionDrone
 
-start_mission_switch = 8
+connect_string = '/dev/serial0'
 
-drone = InspectionDrone('/dev/serial0', baudrate=115200,
+
+drone = InspectionDrone(connection_string=connect_string, baudrate=115200,
                         two_way_switches=[7, 8], three_way_switches=[5, 6, 9, 10, 11, 12])
 
-last_flight_mode = drone.vehicle.mode
-test_time=4
-total_test_time = 4 * 2 + test_time
 
-while not drone.mission_running():
+# if drone.switches[start_mission_switch].is_up():
+#     start_time = time.time()
+#     time_elapsed = time.time() - start_time
+#     drone.launch_mission()
+#     print("Starting mission")
+
+
+last_flight_mode = drone.vehicle.mode
+not_over = True
+
+while not_over:
+    print(drone.is_in_guided_mode())
     drone.update_switch_states()
-    if drone.switches[start_mission_switch].is_up():
-        start_time = time.time()
-        time_elapsed = time.time() - start_time
-        drone.launch_mission()
-        print("Starting mission")
-while time_elapsed < total_test_time:
-    time_elapsed = time.time() - start_time
-    if time_elapsed < 4+test_time:
-        drone.send_mavlink_go_forward(0.5)
-    if 4+test_time < time_elapsed < total_test_time:
-        drone.send_mavlink_go_backward(0.5)
+    if drone.is_in_guided_mode():
+        print("Going North for 10 s at 0.1 m/s")
+        for _ in range(10):
+            drone.send_mavlink_go_forward(0.1)
+            time.sleep(1)
+        drone.set_flight_mode("GUIDED")
+        print("Going South for 10 s at 0.1 m/s")
+        for _ in range(10):
+            drone.send_mavlink_go_backward(0.1)
+            time.sleep(1)
+        drone.set_flight_mode(last_flight_mode)
+        not_over = False
+
+drone.vehicle.close()
