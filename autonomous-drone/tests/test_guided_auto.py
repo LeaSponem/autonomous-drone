6f38d7d0b2e76366744c535b78426cf6bb46fbb1
LeaSponem <leasponem1@gmail.com>
2022-03-24 10:07:11 +0100

    Added mavlink instructions

--- autonomous-drone/tests/test_guided_auto.py
+++ autonomous-drone/tests/test_guided_auto.py
@@ -6,10 +6,21 @@ from inspection_drone import InspectionDrone
 drone = InspectionDrone('/dev/serial0', baudrate=115200,
                         two_way_switches=[7, 8], three_way_switches=[5, 6, 9, 10, 11, 12])
 
+
 while True:
+    # print(drone.is_in_guided_mode())
+    drone.update_switch_states()
+    drone.update_time()
     if drone.is_in_guided_mode():
         print("Petite pause")
-        for _ in range(5):
+        for _ in range(2):
+            drone.send_mavlink_stay_stationary()
+            time.sleep(1)
+        for _ in range(6):
+            drone.send_mavlink_go_forward(0.5)
+            time.sleep(1)
+        for _ in range(2):
             drone.send_mavlink_stay_stationary()
             time.sleep(1)
         drone.set_flight_mode("AUTO")
+    time.sleep(0.1)
