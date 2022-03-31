ca8b5b27e1b22b2a1aa63a5d5f55787194fcec3a
LeaSponem <leasponem1@gmail.com>
2022-03-31 11:12:35 +0200

    Modified obstacle detection test

--- autonomous-drone/tests/read_sensor.py
+++ autonomous-drone/tests/read_sensor.py
@@ -9,23 +9,25 @@ drone = InspectionDrone('/dev/serial0',
                         baudrate=115200,
                         two_way_switches=[7, 8],
                         three_way_switches=[5, 6, 8, 9, 10, 11, 12],
-                        buzzer_pin=23, lidar_address=0x10, critical_distance_lidar=300)
+                        buzzer_pin=23, lidar_address=0x10, critical_distance_lidar=400)
 
-total_test_time = 30
-switch_obstacle = 9
+
 drone.launch_mission()
-obstacle_detected = False
+drone.update_last_flight_mode()
 
 while drone.mission_running():
-    print(drone._lidar.get_distance())
     drone.update_time()
     drone.update_switch_states()
-    drone.update_detection(use_lidar=True, debug=False)
-    if drone.obstacle_detected() and drone._lidar.get_distance()>0 and not obstacle_detected:
-        drone.set_guided_mode()
-        print("Obstacle detected")
-        obstacle_detected = True
-    if not drone.obstacle_detected() and drone._lidar.get_distance()>0 and obstacle_detected:
-        obstacle_detected = False
-        print("No obstacle")
+    if drone.do_lidar_reading():  # ask a reading every 20 ms
+        drone.update_detection(use_lidar=True, debug=True)  # update obstacle detection and print read distance
+    if drone.obstacle_detected():
+        if not drone.is_in_guided_mode():  # first time the obstacle is detected
+            print("Obstacle detected")
+            drone.update_last_flight_mode()  # store value of the last flight mode
+            drone.set_guided_mode()
+    if not drone.obstacle_detected():
+        if drone.is_in_guided_mode():
+            if drone.time_since_last_obstacle_detected() > 5:
+                print("Resume mission")
+                drone.set_flight_mode(drone.get_last_flight_mode())
     time.sleep(0.1)
