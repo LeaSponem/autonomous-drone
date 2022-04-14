c128032b5b5ed7f80cf6f468e38e22965f70f84d
LeaSponem <leasponem1@gmail.com>
2022-04-14 14:52:15 +0200

    Compute intersection between drone and wall

--- autonomous-drone/obstacles/wall.py
+++ autonomous-drone/obstacles/wall.py
@@ -11,15 +11,17 @@ class WallObstacle(Obstacle):
     def get_obstacle_origin(self):
         return self._x_origin, self._y_origin
 
-    def get_obstacle_position(self):
+    def obstacle_equation(self):
         x_0, y_0 = self.get_obstacle_origin()
-        x = self._dimension*np.cos(self._angle)
-        y = self._dimension * np.sin(self._angle)
-        return x_0+x,y_0+y
+        a = np.sin(self._angle)
+        b = y_0 - a*x_0
+        return a, b
 
-    def obstacle_equation(self, x):
-        x_0, y_0 = self.get_obstacle_origin()
-        x_f, y_f = self.get_obstacle_position()
-        a = (y_f - y_0)/(x_f - x_0)
-        b = (y_0*x_f - y_f*x_0)/(x_f - x_0)
-        return a*x+b
+    def intersection(self, x_drone, y_drone, angle_drone):
+        a_wall, b_wall = self.obstacle_equation()
+        a_drone = np.sin(angle_drone)
+        b_drone = y_drone - x_drone*np.sin(angle_drone)
+        a_wall = np.sin(self._angle)
+        x_intersection = (b_drone - b_wall)/(a_wall - a_drone)
+        y_intersection = a_wall*x_intersection+b_wall
+        return x_intersection, y_intersection
