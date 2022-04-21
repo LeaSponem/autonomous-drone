9e6ab429f5fd25903b4513c89e53d78681be6baa
LeaSponem <leasponem1@gmail.com>
2022-04-21 09:57:12 +0200

    Fix angle measure

--- autonomous-drone/drone/virtual_drone.py
+++ autonomous-drone/drone/virtual_drone.py
@@ -13,22 +13,18 @@ class VirtualDrone(InspectionDrone):
                                  two_way_switches, three_way_switches, critical_distance_lidar=1)
         self._drone_x = 0
         self._drone_y = 0
-        self._yaw = self.vehicle.attitude.yaw
-        self._initial_angle = (180/np.pi)*(np.pi/2 + self._yaw)
+        self._update_yaw()
+        self._initial_angle = 90 + self._yaw
         self._location = self.vehicle.location.global_relative_frame
         self._local_frame = self._init_local_frame()
         self._lidar = VirtualTFMiniPlus(critical_distance_lidar)
 
     def _init_local_frame(self):
-        local_frame = SimulationPosition(self._location.lat, self._location.lon,
-                                         (180 / np.pi) * (np.pi / 2 + self._yaw))
+        local_frame = SimulationPosition(self._location.lat, self._location.lon, self._initial_angle)
         return local_frame
 
     def _update_location(self):
         self._location = self.vehicle.location.global_relative_frame
-
-    def _update_yaw(self):
-        self._yaw = self.vehicle.attitude.yaw
 
     def _update_virtual_position(self):
         self._update_location()
