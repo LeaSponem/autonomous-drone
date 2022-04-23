eb0ab7bba7e0af122d6bc29f9d9d0193f5fb8fe3
LeaSponem <leasponem1@gmail.com>
2022-04-23 16:52:19 +0200

    Clean and comment VirtualDrone class

--- autonomous-drone/drone/virtual_drone.py
+++ autonomous-drone/drone/virtual_drone.py
@@ -1,4 +1,3 @@
-import numpy as np
 import time
 import sys
 from inspection_drone import InspectionDrone
@@ -8,25 +7,44 @@ from virtual_tf_mini import VirtualTFMiniPlus
 
 
 class VirtualDrone(InspectionDrone):
+    """
+    Specific class for a virtual drone used on a simulator
+    Deprecated class from InspectionDrone with parameters and methods for virtual positions
+    """
     def __init__(self, connection_string, baudrate, two_way_switches, three_way_switches, critical_distance_lidar=1):
         InspectionDrone.__init__(self, connection_string, baudrate,
                                  two_way_switches, three_way_switches, critical_distance_lidar=1)
         self._drone_x = 0
         self._drone_y = 0
+        # GPS coordinates
+        self._location = self.vehicle.location.global_relative_frame
+        # Initial angle between X axis and the North
         self._update_yaw()
         self._initial_angle = 90 + self._yaw
-        self._location = self.vehicle.location.global_relative_frame
+        # Init local frame for simulated positions
         self._local_frame = self._init_local_frame()
         self._lidar = VirtualTFMiniPlus(critical_distance_lidar)
 
     def _init_local_frame(self):
+        """
+        Initialize the local frame used to create virtual drone positions
+        The positions are given relatively to the initial latitude and longitude of the drone
+        """
         local_frame = SimulationPosition(self._location.lat, self._location.lon, self._initial_angle)
         return local_frame
 
     def _update_location(self):
+        """
+        Update the GPS coordinates of the drone
+        Return (latitude, longitude, altitude)
+        """
         self._location = self.vehicle.location.global_relative_frame
 
     def _update_virtual_position(self):
+        """
+        Convert the GPS coordinates to x and y coordinates relatively to the drone home position
+        Convert the coordinates in meters
+        """
         self._update_location()
         self._drone_x, self._drone_y = self._local_frame.get_position(self._location)
         self._drone_x *= 0.001
@@ -38,10 +56,16 @@ class VirtualDrone(InspectionDrone):
         return self._initial_angle - self._yaw
 
     def get_virtual_position(self):
+        """
+        Update and return the drone virtual position
+        """
         self._update_virtual_position()
         return self._drone_x, self._drone_y
 
     def update_detection(self, use_lidar=True, debug=False, walls=None):
+        """
+        Read the distance returned by the sensor and return if an obstacle is detected
+        """
         self._update_virtual_position()
         if self._lidar.read_distance(self._drone_x, self._drone_y, self.get_angle(), walls) and debug:
             print("Lidar range:" + str(self._lidar.get_distance()))
@@ -53,6 +77,10 @@ class VirtualDrone(InspectionDrone):
             self._obstacle_detected = False
 
     def arm_and_takeoff(self, tgt_altitude):
+        """
+        Function from the dronekit documentation used to arm and takeoff the drone on the simulator
+        Wait for the drone to reach the target altitude
+        """
         print("Arming motors")
         while not self.vehicle.is_armable:
             time.sleep(1)
