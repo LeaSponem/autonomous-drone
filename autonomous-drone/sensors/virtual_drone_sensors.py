986b4a6588f9f05e857247d097a89968663e78ba
LeaSponem <leasponem1@gmail.com>
2022-05-10 17:31:10 +0200

    Fix update detection

--- autonomous-drone/sensors/virtual_drone_sensors.py
+++ autonomous-drone/sensors/virtual_drone_sensors.py
@@ -40,22 +40,3 @@ class VirtualThreeLidarSensorsDetection(ThreeLidarSensorsDetection):
 
     def read_left_distance(self, drone_x, drone_y, angle, walls):
         return self._left_lidar.read_distance(drone_x, drone_y, angle, walls)
-
-    def update_right_detection(self, debug=False):
-        if self.read_right_distance() and debug:
-            print("Right lidar range:" + str(self._right_lidar.get_distance()))
-        if self._right_lidar.critical_distance_reached():
-            self._obstacle_detected_right = True
-        else:
-            self._obstacle_detected_right = False
-
-    def update_left_detection(self, debug=False):
-        if self.read_left_distance() and debug:
-            print("Left lidar range:" + str(self._left_lidar.get_distance()))
-        if self._left_lidar.critical_distance_reached():
-            self._obstacle_detected_left = True
-        else:
-            self._obstacle_detected_left = False
-
-
-sensors = VirtualThreeLidarSensorsDetection(lidar_angle=[0, 90, -90])
