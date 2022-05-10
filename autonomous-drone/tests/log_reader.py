6fbbfcd18e8ef6f7b5216942d1f8fc7f970a9c69
Naxosia <clara.naxosia@gmail.com>
2022-05-10 13:27:37 +0200

    Corrected the code, I forgot to write \n at the end of each string

--- autonomous-drone/tests/log_reader.py
+++ autonomous-drone/tests/log_reader.py
@@ -2,7 +2,11 @@
 This code is to plot the logs produced by the code "test_stop_auto" in IRL Mode
 """
 
-def read_and_plot(name):
+import matplotlib.pyplot as plt
+
+def read_and_plot(name, K):
+    
+    target_distance = 200      #The drone must stop at this distance from the obstacle
 
     "------ List of the data ------"
     list_V_command = []
@@ -15,22 +19,37 @@ def read_and_plot(name):
 
     doc = open(name, 'r')
     data = doc.readline()       #The first line should be "Time"
-    assert data == "Time"
+    
+    assert data == "Time \n"
+    
+    data = doc.readline()
 
-    while data =! "V_command":
-        list_time.append(float(data))
+    while data != "V_command \n":
+        list_time.append(float(data[:-1]))
         data = doc.readline()
+        
+    data = doc.readline()
 
-    while data =! "V_measured":
-        list_V_measured.append(float(data))
+    while data != "V_measured \n":
+        list_V_command.append(float(data[:-1]))
         data = doc.readline()
+    
+    data = doc.readline()
 
-    while data =! "measured_distance":
-        list_measured_distance.append(float(data))
+    while data != "measured_distance \n":
+        list_V_measured.append(float(data[:-1]))
         data = doc.readline()
+    
+    data = doc.readline()
 
-    while data=!"":
-        list_yaw.append(float(data))
+    while data != "yaw \n":
+        list_measured_distance.append(float(data[:-1]))
+        data = doc.readline()
+    
+    data = doc.readline()
+    
+    while data != "":
+        list_yaw.append(float(data[:-1]))
         data = doc.readline()
 
     doc.close()
@@ -45,12 +64,20 @@ def read_and_plot(name):
     axes[0].set_xlabel('Time')
     axes[0].set_ylabel("Speed on x")
     axes[0].legend()
-
+    
     axes[1].plot(list_time,list_measured_distance)
     axes[1].plot([list_time[0],list_time[-1]],[target_distance,target_distance])
     axes[1].set_xlabel("Time")
     axes[1].set_ylabel("Measured Distance")
 
+    
+    """ Rescale the graph
+    stop = int(len(list_time)/3)
+    axes[1].plot(list_time[1:stop],list_measured_distance[1:stop])
+    axes[1].plot([list_time[0],list_time[stop]],[target_distance,target_distance])
+    axes[1].set_xlabel("Time")
+    axes[1].set_ylabel("Measured Distance")
+    """
     plt.title(title)
     plt.show()
 
