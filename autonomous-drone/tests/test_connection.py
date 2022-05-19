c009f3b395b5149dbdc44121be15b1ee8a99e060
LeaSponem <leasponem1@gmail.com>
2022-05-19 10:30:43 +0200

    Update test_connection with sensors

--- autonomous-drone/tests/test_connection.py
+++ autonomous-drone/tests/test_connection.py
@@ -6,7 +6,9 @@ from inspection_drone import InspectionDrone
 
 # Connection between the Raspberry and the Pixhawk
 drone = InspectionDrone('/dev/serial0', baudrate=115200,
-                        two_way_switches=[7, 8], three_way_switches=[5, 6, 9, 10, 11, 12])
+                        two_way_switches=[7, 8], three_way_switches=[5, 6, 9, 10, 11, 12],
+                        lidar_address=[0x10], lidar_angle=[0],
+                        critical_distance_lidar=200)
 
 # Access to parameters
 for _ in range(10):
